bb2bd311ea068ea582ad411b1f55afe7213b46d5
Alessandro Riva <alessandro.riva@techedgegroup.com>
2021-05-08 15:38:21 +0200

    Aggiunto swagger

--- JavaSpringBoot/webapi-backend/src/main/java/com/te/accademy/webapi/restcontroller/CaseDistroController.java
+++ JavaSpringBoot/webapi-backend/src/main/java/com/te/accademy/webapi/restcontroller/CaseDistroController.java
@@ -18,6 +18,8 @@ import com.te.accademy.webapi.datamodel.CaseDistribution;
 import com.te.accademy.webapi.datamodel.CaseSummary;
 import com.te.accademy.webapi.datamodel.Country;
 
+import io.swagger.annotations.ApiOperation;
+
 @RestController
 public class CaseDistroController {
 
@@ -31,21 +33,24 @@ public class CaseDistroController {
 		}
 	}
 
+	@ApiOperation(value = "List all countries")
 	@GetMapping("/countries")
 	public List<Country> getAll() {
-		Country c=new Country();
+		Country c = new Country();
 		c.setCode("ITA");
 		c.setContinent("Europe");
 		c.setCountry("Italy");
-		
+
 		return Collections.singletonList(c);
 	}
 
+	@ApiOperation(value = "Get CaseDistribution entry")
 	@GetMapping("/case/{case_id}")
 	public CaseDetail getCaseById(@PathVariable Integer case_id) {
 		return null;
 	}
 
+	@ApiOperation(value = "Search CaseDistribution entries")
 	@GetMapping("/case")
 	public List<CaseDetail> getDetail(//
 			@RequestParam(required = false) Date from, //
@@ -55,24 +60,28 @@ public class CaseDistroController {
 		return Collections.<CaseDetail>emptyList();
 	}
 
+	@ApiOperation(value = "Insert a new CaseDistribution")
 	@PostMapping("/case")
 	public RestResponse insertCase(@RequestBody CaseDistribution newCaseDistribution) {
 
 		return new RestResponse("Success", "Case created successfully");
 	}
 
+	@ApiOperation(value = "Update a CaseDistribution")
 	@PutMapping("/case")
 	public RestResponse updateCase(@RequestBody CaseDistribution updateCaseDistribution) {
 
 		return new RestResponse("Success", "Case updated successfully");
 	}
 
+	@ApiOperation(value = "Delete a CaseDistribution")
 	@DeleteMapping("/case")
 	public RestResponse deleteCase(@RequestParam Integer caseId) {
 
 		return new RestResponse("Success", "Case deleted successfully");
 	}
 
+	@ApiOperation(value = "Get CaseDistribution summary")
 	@GetMapping("/case-summary")
 	public List<CaseSummary> getSummary(//
 			@RequestParam(required = false) Date from, //
